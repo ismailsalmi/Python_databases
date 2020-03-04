e0cdfe3bb01164f8c0df6df7004335d830147142
ismail <ismail.man.2013@gmail.com>
2020-03-04 22:30:20 +0100

    firabase crud

---
+++ firebase_database.py
@@ -0,0 +1,44 @@
+from base64 import main
+from pyrebase import initialize_app
+
+class Firebase:
+    # firebase connection
+   config = {
+        "apiKey": "AIzaSyBhyJ6gALd9V1c_eQipCfsYyUB2mSz0slQ",
+        "authDomain": "mysite-b3d04.firebaseapp.com",
+        "databaseURL": "https://mysite-b3d04.firebaseio.com",
+        "storageBucket": "mysite-b3d04.appspot.com",
+    }
+
+   firebase = initialize_app(config)
+   db = firebase.database()
+   users = db.child("users")
+   # insert
+   insert = lambda self,username, name, lname, age: self.users.child(
+       username).set({"name": name, "lname": lname, "age": age})
+   # update
+   update = lambda self,username, name, lname, age: self.users.child(
+       username).update({"name": name,"lname": lname, "age": age})
+   # delete
+   delete = lambda self, username: self.users.child(username).remove()
+   # read
+   read = lambda self: self.users.get()
+if __name__=="__main__":
+    firebase = Firebase()
+    # insert
+    # insert = firebase.insert(username='hnsgf3', name='ismail', lname='salmi', age=24)
+    # print(insert, 'inserted')
+    # update
+    # update = firebase.update(username='qlmys1', name='samir', lname='salmi', age=24)
+    # print(update, 'updated')
+    # delete
+    # delete = firebase.delete(username='hnsgf3')
+    # is_deleted = 'Deleted' if delete else 'Not deleted'
+    # print(is_deleted)
+    # read
+    read = firebase.read().each()
+    for user in read:
+        print(user.val()['name'])
+    main()
+
+
